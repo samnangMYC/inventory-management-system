2849b8d2ed8d8daf737d755d6464814a388351bc
Samnang <140832092+samnangMYC@users.noreply.github.com>
2024-12-26 09:17:07 +0700

    rollback

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="">
+
     <div class="flex px-4 py-2 justify-between">
         <h1 class="text-3xl font-medium text-gray-700 dark:text-white">Add Product Form</h1>
     </div>
@@ -13,8 +13,9 @@
         <div class="py-2 my  overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
             <div class="p-4  inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
                 <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add a new product</h2>
-                <form action="{{ route('product.store')}}" method="POST" enctype="multipart/form-data">
+                <form action="{{ route('product.store')}}"  method="POST" enctype="multipart/form-data">
                     @csrf
+                    @method('POST')
                     <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                         <div class="sm:col-span-2">
                             <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Name</label>
@@ -124,5 +125,4 @@
             preview.src = '';
         }
     </script>
-</div>
 @endsection
